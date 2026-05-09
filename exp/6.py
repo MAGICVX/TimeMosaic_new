19b99dee2f738a2a0a0012fcca8520860e1df67f
mike <2487635413@qq.com>
2026-05-09 21:37:39 +0800

    添加实验文件5、6和新实验脚本，以及模型5

---
+++ exp/6.py
@@ -0,0 +1,362 @@
+from data_provider.data_factory import data_provider
+from exp.exp_basic import Exp_Basic
+from utils.tools import EarlyStopping, adjust_learning_rate, visual
+from utils.metrics import metric
+import torch
+import torch.nn as nn
+from torch import optim
+import os
+import time
+import warnings
+import numpy as np
+from utils.dtw_metric import dtw, accelerated_dtw
+from utils.augmentation import run_augmentation, run_augmentation_single
+import torch.nn.functional as F
+
+
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
+warnings.filterwarnings('ignore')
+
+
+class Exp_TimeMosaic(Exp_Basic):
+    def __init__(self, args):
+        super(Exp_TimeMosaic, self).__init__(args)
+
+    def _build_model(self):
+        model = self.model_dict[self.args.model].Model(self.args).float()
+
+        if self.args.use_multi_gpu and self.args.use_gpu:
+            model = nn.DataParallel(model, device_ids=self.args.device_ids)
+        return model
+
+    def _get_data(self, flag):
+        data_set, data_loader = data_provider(self.args, flag)
+        return data_set, data_loader
+
+    def _select_optimizer(self):
+        model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
+        return model_optim
+
+    def _select_criterion(self):
+        criterion = nn.L1Loss()
+        return criterion
+ 
+
+    def vali(self, vali_data, vali_loader, criterion):
+        total_loss = []
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(vali_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float()
+
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+                # encoder - decoder
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        outputs, cls_pred = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[:2]
+                else:
+                    outputs, cls_pred = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[:2]
+                f_dim = -1 if self.args.features == 'MS' else 0
+                outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+
+                pred = outputs.detach().cpu()
+                true = batch_y.detach().cpu()
+
+                loss = criterion(pred, true)
+
+                total_loss.append(loss)
+        total_loss = np.average(total_loss)
+        self.model.train()
+        return total_loss
+
+    def train(self, setting):
+        train_data, train_loader = self._get_data(flag='train')
+        vali_data, vali_loader = self._get_data(flag='val')
+        test_data, test_loader = self._get_data(flag='test')
+
+        path = os.path.join(self.args.checkpoints, setting)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        time_now = time.time()
+
+        train_steps = len(train_loader)
+        early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
+
+        model_optim = self._select_optimizer()
+        criterion = self._select_criterion()
+
+        if self.args.use_amp:
+            scaler = torch.cuda.amp.GradScaler()
+
+        accumulation_steps = getattr(self.args, 'accumulation_steps', 1)
+
+        for epoch in range(self.args.train_epochs):
+            iter_count = 0
+            train_loss = []
+
+            self.model.train()
+            epoch_time = time.time()
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
+                iter_count += 1
+                # Only zero gradients at the start of accumulation cycle
+                if (i + 1) % accumulation_steps == 0 or (i + 1) == train_steps:
+                    model_optim.zero_grad()
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+                
+                if self.args.mask_ratio > 0:
+                    B, T, C = batch_x.shape
+                    mask = torch.rand(B, T, C, device=batch_x.device) < self.args.mask_ratio
+                elif self.args.mask_ratio_patch > 0:
+                    patch_num = int((self.args.seq_len - self.args.patch_len) / self.args.stride + 2)
+                    B, T, C = batch_x.shape
+                    mask = (torch.rand(B * C, patch_num, device=batch_x.device) < self.args.mask_ratio_patch)
+                    mask = mask.unsqueeze(-1).expand(-1, -1, self.args.d_model)
+                else:
+                    mask = None
+                    
+                # encoder - decoder
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        result = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark, mask, batch_y)
+                        outputs, cls_pred, dec_mask = result
+
+                        if self.args.model == "TimeMosaic":
+                            cls_soft = self.model.patch_embedding.latest_cls_soft
+                            current_ratio = cls_soft.mean(dim=0)
+                        else:
+                            counts = torch.bincount(cls_pred, minlength=3).float()
+                            current_ratio = counts / counts.sum()
+
+                        f_dim = -1 if self.args.features == 'MS' else 0
+                        outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                        batch_y_trim = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+                        lambda_cls = 0.01
+                        loss = criterion(outputs, batch_y_trim) + lambda_cls * criterion(
+                            current_ratio, torch.full_like(current_ratio, 1.0 / len(current_ratio)).detach())
+                        if self.args.mask_ratio > 0:
+                            loss = loss + criterion(batch_x, dec_mask)
+                        train_loss.append(loss.mean().item())
+                else:
+                    result = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark, mask, batch_y)
+                    outputs, cls_pred, dec_mask = result
+
+
+                    if self.args.model == "TimeMosaic":
+                        cls_soft = self.model.patch_embedding.latest_cls_soft  # [N, num_classes]
+                        current_ratio = cls_soft.mean(dim=0)
+                    else:
+                        counts = torch.bincount(cls_pred, minlength=3).float()
+                        current_ratio = counts / counts.sum()
+
+                    target_ratio = torch.full_like(current_ratio, 1.0 / len(current_ratio)).detach()
+                    loss_reg = criterion(current_ratio, target_ratio)
+
+                    f_dim = -1 if self.args.features == 'MS' else 0
+                    outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                    batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+
+                    lambda_cls = 0.001
+                    if self.args.mask_ratio > 0:
+                        loss = criterion(outputs, batch_y) + lambda_cls * loss_reg + lambda_cls * criterion(batch_x[mask], dec_mask[mask])
+                    else:
+                        loss = criterion(outputs, batch_y) + lambda_cls * loss_reg
+
+                    train_loss.append(loss.mean().item())
+
+                if (i + 1) % 100 == 0:
+                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.mean().item()))
+                    speed = (time.time() - time_now) / iter_count
+                    left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
+                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                    iter_count = 0
+                    time_now = time.time()
+
+                # Scale loss for gradient accumulation
+                loss = loss / accumulation_steps
+
+                if self.args.use_amp:
+                    scaler.scale(loss).backward()
+                    if (i + 1) % accumulation_steps == 0 or (i + 1) == train_steps:
+                        scaler.step(model_optim)
+                        scaler.update()
+                else:
+                    loss.mean().backward()
+                    if (i + 1) % accumulation_steps == 0 or (i + 1) == train_steps:
+                        model_optim.step()
+
+            print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
+            train_loss = np.average(train_loss)
+            vali_loss = self.vali(vali_data, vali_loader, criterion)
+            test_loss = self.vali(test_data, test_loader, criterion)
+
+            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
+                epoch + 1, train_steps, train_loss, vali_loss, test_loss))
+            early_stopping(vali_loss, self.model, path)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+            
+            adjust_learning_rate(model_optim, epoch, self.args)
+
+        best_model_path = path + '/' + 'checkpoint.pth'
+        self.model.load_state_dict(torch.load(best_model_path))
+
+        return self.model
+    
+    def test(self, setting, test=0):
+        test_data, test_loader = self._get_data(flag='test')
+        if test:
+            print('loading model')
+            self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + setting, 'checkpoint.pth')))
+
+        preds = []
+        trues = []
+        folder_path = './test_results/' + setting + '/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        self.model.eval()
+        all_pred_list = []
+
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(test_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+                # encoder - decoder
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        outputs, cls_pred = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[:2]
+                else:
+                    outputs, cls_pred = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[:2]
+
+                f_dim = -1 if self.args.features == 'MS' else 0
+                outputs = outputs[:, -self.args.pred_len:, :]
+                batch_y = batch_y[:, -self.args.pred_len:, :].to(self.device)
+                outputs = outputs.detach().cpu().numpy()
+                batch_y = batch_y.detach().cpu().numpy()
+                if test_data.scale and self.args.inverse:
+                    shape = batch_y.shape
+                    if outputs.shape[-1] != batch_y.shape[-1]:
+                        outputs = np.tile(outputs, [1, 1, int(batch_y.shape[-1] / outputs.shape[-1])])
+                    outputs = test_data.inverse_transform(outputs.reshape(shape[0] * shape[1], -1)).reshape(shape)
+                    batch_y = test_data.inverse_transform(batch_y.reshape(shape[0] * shape[1], -1)).reshape(shape)
+
+                outputs = outputs[:, :, f_dim:]
+                batch_y = batch_y[:, :, f_dim:]
+                if cls_pred is not None:
+                    all_pred_list.append(cls_pred.cpu())
+
+
+                pred = outputs
+                true = batch_y
+
+                preds.append(pred)
+                trues.append(true)
+                if i % 20 == 0:
+                    input = batch_x.detach().cpu().numpy()
+                    if test_data.scale and self.args.inverse:
+                        shape = input.shape
+                        input = test_data.inverse_transform(input.reshape(shape[0] * shape[1], -1)).reshape(shape)
+                    gt = np.concatenate((input[0, :, -1], true[0, :, -1]), axis=0)
+                    pd = np.concatenate((input[0, :, -1], pred[0, :, -1]), axis=0)
+                    visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'))
+
+        # print(len(all_pred_list))
+        if self.args.counts:
+            if len(all_pred_list) > 0:
+                patch_len_list = eval(self.args.patch_len_list)
+                all_cls_pred = torch.cat(all_pred_list)
+                patch_counts = torch.bincount(all_cls_pred, minlength=len(patch_len_list))
+                print("Granularity counts:")
+                for i, patch_len in enumerate(patch_len_list):
+                    print(f"Patch size {patch_len}: {patch_counts[i].item()} 次")
+
+        preds = np.concatenate(preds, axis=0)
+        trues = np.concatenate(trues, axis=0)
+        print('test shape:', preds.shape, trues.shape)
+        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+        trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
+        print('test shape:', preds.shape, trues.shape)
+
+        # result save
+        folder_path = './results/' + setting + '/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        mae, mse, rmse, mape, mspe, _ = metric(preds, trues)
+        print('mse:{}, mae:{}, rmse:{}, mape:{}, mspe:{}'.format(mse, mae, rmse, mape, mspe))
+        f = open("result_long_term_forecast.txt", 'a')
+        f.write(setting + "  \n")
+        f.write('mse:{}, mae:{}, rmse:{}, mape:{}, mspe:{}'.format(mse, mae, rmse, mape, mspe))
+        f.write('\n')
+        f.write('\n')
+        f.close()
+
+        # np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
+        # np.save(folder_path + 'pred.npy', preds)
+        # np.save(folder_path + 'true.npy', trues)
+        
+        self.profile_model(test_loader)
+        
+        # best_model_path = os.path.join('./checkpoints/' + setting, 'checkpoint.pth')
+        # if os.path.exists(best_model_path):
+        #     os.remove(best_model_path)
+        #     print(f"Deleted model checkpoint at: {best_model_path}")
+
+        return
+    
+    def profile_model(self, test_loader):
+        self.model.eval()
+        with torch.no_grad():
+            batch_x, batch_y, batch_x_mark, batch_y_mark = next(iter(test_loader))
+            batch_x = batch_x.float().to(self.device)
+            batch_y = batch_y.float().to(self.device)
+            batch_x_mark = batch_x_mark.float().to(self.device)
+            batch_y_mark = batch_y_mark.float().to(self.device)
+
+            dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
+            dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+
+            torch.cuda.reset_peak_memory_stats()
+            torch.cuda.synchronize()
+            start_time = time.time()
+
+            _ = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[:2]
+
+            torch.cuda.synchronize()
+            end_time = time.time()
+
+            inference_time = end_time - start_time
+            gpu_mem = torch.cuda.memory_allocated(self.device) / 1024 / 1024
+            peak_mem = torch.cuda.max_memory_allocated(self.device) / 1024 / 1024
+            total_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+
+            print("=" * 80)
+            print("Model Profiling Summary")
+            print(f"{'Total Params':<25}: {total_params:,}")
+            print(f"{'Inference Time (s)':<25}: {inference_time:.6f}")
+            print(f"{'GPU Mem Footprint (MB)':<25}: {gpu_mem:.2f}")
+            print(f"{'Peak Mem (MB)':<25}: {peak_mem:.2f}")
+            print("=" * 80)
