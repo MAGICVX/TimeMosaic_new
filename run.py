ce8b1ceb342659bc0defc8a247f2f226ac6d80a9
mike <2487635413@qq.com>
2026-06-16 12:42:04 +0800

    fix: remove orphan help line from run.py

--- run.py
+++ run.py
@@ -130,7 +130,6 @@ if __name__ == '__main__':
                         help='Number of prefix tokens per layer for K/V injection')
     parser.add_argument('--lam_prefix_moe', type=float, default=0.001,
                         help='Weight of MoE prefix load balancing loss')
-                        help='Number of spectral views (0=disable spectral decomposition)')
     parser.add_argument('--use_prefix', type=int, default=1,
                         help='Enable prefix attention K/V injection (0/1)')
     parser.add_argument('--lam_entropy', type=float, default=0.03,
